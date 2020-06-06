d6c104b909a52a2c1a11eff4eb0b199514c5b2d1
Alexandr <knxandr@rambler.ru>
2020-06-06 15:11:22 +0500

    test1

---
+++ form.py
@@ -0,0 +1,22 @@
+"""
+result = [addr_id, # Идентификатор адреса
+          source, # Исходный ненормализованный адрес
+          level, # Уровень распознания адреса
+          country, # Страна
+          region, # Регион
+          city, # Город
+          place, # Место
+          district_type, # Тип района
+          disctrict, # Район
+          street_type, # Тип улицы
+          street, # Улица
+          house, # Дом
+          building, # Строение
+          korpus, # Корпус
+          flat, # Квартира
+          office ]# Офис
+"""         
+          
+
+def make_result(arr):
+  pass
